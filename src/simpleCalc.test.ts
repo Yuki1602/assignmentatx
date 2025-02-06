9c569db99501746a3eb2584dcaf25eede4ae7a86
Ismail Rangwla <irangwala0@gmail.com>
2025-02-06 17:23:16 +0530

    test: add unit test for single number input in simple string calculator
     FAIL  src/simpleCalc.test.ts
      Simple String Calculator
        √ should pass if function returns 0 for an empty string (1 ms)
        × should pass if function returns the number itself if only one number is provided (2 ms)
    
      ● Simple String Calculator › should pass if function returns the number itself if only one number is provided

--- src/simpleCalc.test.ts
+++ src/simpleCalc.test.ts
@@ -4,4 +4,8 @@ describe("Simple String Calculator", () => {
   test("should pass if function returns 0 for an empty string", () => {
     expect(add("")).toBe(0);
   });
+
+  test("should pass if function returns the number itself if only one number is provided", () => {
+    expect(add("1")).toBe(1);
+  });
 });
