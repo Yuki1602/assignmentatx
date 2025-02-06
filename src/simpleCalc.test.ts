05c555d32233b8bbcafbb096acc8edc29e2b599a
Ismail Rangwla <irangwala0@gmail.com>
2025-02-06 23:53:43 +0530

    test: add unit test to verify handling of delimiters of any length in add function
     FAIL  src/simpleCalc.test.ts
      ● Simple String Calculator › should handle delimiters of any length
    
        SyntaxError: Invalid regular expression: /***/: Nothing to repeat
            at new RegExp (<anonymous>)
    
          14 |   if (numbers.startsWith("//")) {
          15 |     const delimiterEndIndex = numbers.indexOf("\n");
        > 16 |     delimiter = new RegExp(numbers.slice(2, delimiterEndIndex).replace(/[\[\]]/g, '')); // Extract delimiter and remove square brackets
             |                 ^
          17 |
          18 |     // Remove the custom delimiter line from the string
          19 |     numbers = numbers.slice(delimiterEndIndex + 1);
    
          at add (src/simpleCalc.ts:16:17)
          at Object.<anonymous> (src/simpleCalc.test.ts:37:15)
    
    A worker process has failed to exit gracefully and has been force exited. This is likely caused by tests leaking due to improper teardown. Try running with --detectOpenHandles to find leaks. Active timers can also cause this, ensure that .unref() was called on them.
    Test Suites: 1 failed, 1 passed, 2 total
    Tests:       1 failed, 9 passed, 10 total
    Snapshots:   0 total
    Time:        4.152 s
    Ran all test suites.

--- src/simpleCalc.test.ts
+++ src/simpleCalc.test.ts
@@ -32,4 +32,8 @@ describe("Simple String Calculator", () => {
   test("should pass if function ignores numbers greater than 1000", () => {
     expect(add("2,1001")).toBe(2);
   });
+
+  test("should pass if function handles delimiters of any length", () => {
+    expect(add("//[***]\n1***2***3")).toBe(6);
+  });
 });
