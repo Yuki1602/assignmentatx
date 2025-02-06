a3205cc013c519665e21e3e28d3f8ad618723dee
Ismail Rangwla <irangwala0@gmail.com>
2025-02-06 17:36:33 +0530

    feat: simplify add function to calculate sum of multiple comma-separated numbers
     PASS  src/simpleCalc.test.ts
      Simple String Calculator
        √ should pass if function returns 0 for an empty string (1 ms)
        √ should pass if function returns the number itself if only one number is provided
        √ should pass if function returns the sum of two comma-separated numbers
        √ should pass if function returns the sum of multiple numbers (1 ms)
    
    Test Suites: 1 passed, 1 total
    Tests:       4 passed, 4 total
    Snapshots:   0 total
    Time:        0.358 s, estimated 1 s

--- src/simpleCalc.ts
+++ src/simpleCalc.ts
@@ -7,12 +7,10 @@
 export function add(numbers: string): number {
     if (!numbers) return 0; // this will pass the first test case 
     const numArray = numbers.split(",").map(Number);
-    if (numArray.length === 1) {
-        return numArray[0];  // this will pass the second test case
+    let sum = 0;
+    for (let i = 0; i < numArray.length; i++) { // this will pass the rest of the test cases
+      sum += numArray[i];
     }
-
-    if (numArray.length === 2) {
-        return numArray[0] + numArray[1];  // this will pass the third test case
-    }
-    return 0;
+  
+    return sum;
 }
